6a214480f6598fb300059242fede69933eb110a2
GurnamSingh1 <46693284+GurnamSingh1@users.noreply.github.com>
2019-01-14 17:09:40 -0500

    Week 2 class work

---
+++ Script.js
@@ -0,0 +1,15 @@
+// Name : Gurnam Singh
+// Date : January 14, 2019
+// Student id : C0741350
+// Course: CSD 2214 Section 6
+alert("Java script programming")
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('clock');
+const daySpan = document.querySelector('.days')
+const hourspan = document.querySelector('.hours')
+const mintuesSpan = document.querySelector('.mintues')
+const secondsSpan = document.querySelector('.seconds')
+console.log("Hii!!!");
+console.log(endDate.value);
